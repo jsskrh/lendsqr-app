8709271bf95234c7636c2d07c2b29039166e692d
Jesse Akorah <jesseakorah@gmail.com>
2022-10-14 00:30:27 +0100

    Create users dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
+import Users from "./Users";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/users" element={<Users />} />
         <Route path="/*" element={<Home />} />
       </Routes>
     </div>
---
+++ src/Users.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const Users = () => {
+  return (
+    <div className="users">
+      <Header />
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Users;
